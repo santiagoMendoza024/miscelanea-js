c9c5a5d96b630e950292b8bfb7d41591a91cf0af
Santiago Mendoza <escoem@outlook.com>
2022-02-19 20:56:58 -0600

    var

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,17 @@
+/* Variables
+    var es una palabra reservada de javascript
+*/
+var producto = "Audifonos Gamer"; // Iniciar variable y asignarle valor
+var disponible; //iniciamos la variable pero sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+ disponible = true;
+
+ var producto1 = "Computadora",
+    disponibilidad = true;
+    categoria = 'Computadoras'
+
+// Estilos para las variables
+var nombre_producto = ' Monitor HD'; // underscore
+var nombreProducto = "Monitor HD" // Camelcase
