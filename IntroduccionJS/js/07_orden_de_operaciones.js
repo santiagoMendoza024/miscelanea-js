7910a8efa20901d5d48de505b05b9038e9dfa12c
Santiago Mendoza <escoem@outlook.com>
2022-02-22 17:29:10 -0600

    Números

--- IntroduccionJS/js/07_orden_de_operaciones.js
+++ IntroduccionJS/js/07_orden_de_operaciones.js
@@ -9,3 +9,17 @@ resultado = (20 + 30) * 5;//En este caso se resuleve primero el parentecis
 resultado = (100 + 200 + 300) * .2;// calculamos el 20% de 3 articulos, primero se suma el total y despues se saca el porcentaje
 
 console.log(resultado);
+
+/**
+ * Incrementos
+ */
+let puntaje = 10;
+puntaje++;//incrementar en 1
+++puntaje;//primero suma y despues lo muestra
+
+puntaje--//muestra y resta
+--puntaje//resta y despues muestra
+
+// += es para añadirle otra cantidad puntaje += 10 = 20; puntaje++ = 11
+
+console.log(puntaje);
