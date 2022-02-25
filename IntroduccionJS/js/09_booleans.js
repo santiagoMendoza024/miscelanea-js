962e26de79a158e0c6701fb37e65332b64b7a647
Santiago Mendoza <escoem@outlook.com>
2022-02-25 14:05:24 -0600

    booleans

---
+++ IntroduccionJS/js/09_booleans.js
@@ -0,0 +1,11 @@
+/**
+ * Bolleanos - Solo pueden tener dos valores true o false
+ * 
+ */
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+//typeof nos ayuda a ver el tipo de dato
