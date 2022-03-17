de6ed34053480417f91f9cb8bcf126081fd08912
Santiago Mendoza <escoem@outlook.com>
2022-03-17 09:53:09 -0600

    update

---
+++ JS_Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,8 @@
+//alert("externo")
+// Pregunta al usuario su nombre
+const nombre = prompt("Cual es tu name");
+// Modifica el contenido y le agrega nuevo
+document.querySelector('.contenido').innerHTML = `${nombre} anda aprendiendo js`;
+
+// Asignar valor a la constante 
+const producto = "Monitor HD";
